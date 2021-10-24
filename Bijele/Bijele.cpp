5b9231e3a6830b6067ffbb60e691c59411312255
TheSebgon <92965117+TheSebgon@users.noreply.github.com>
2021-10-24 17:20:08 +0200

    Add files via upload

---
+++ Bijele/Bijele.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+    int full_set[6]{ 1,1,2,2,2,8 };
+    int given_set[6];
+    for (int i = 0; i < 6; i++)
+        std::cin >> given_set[i];
+    for (int i = 0; i < 6; i++)
+        std::cout << full_set[i] - given_set[i] << ' ';
+}
