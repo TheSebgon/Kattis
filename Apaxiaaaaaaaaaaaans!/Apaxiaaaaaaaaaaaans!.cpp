cb50446867185f60125b79d3060f29e8aa2156da
TheSebgon <92965117+TheSebgon@users.noreply.github.com>
2021-10-29 13:10:53 +0200

    Add files via upload

---
+++ Apaxiaaaaaaaaaaaans!/Apaxiaaaaaaaaaaaans!.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+int main() 
+{
+    std::string ap_name;
+    std::cin >> ap_name;
+    for (int i = 0; i < ap_name.length() - 1; i++) 
+    {
+        if (ap_name[i] != ap_name[i + 1]) 
+        {
+            std::cout << ap_name[i];
+        }
+    }
+    std::cout << ap_name[ap_name.length() - 1];
+    return 0;
+}
