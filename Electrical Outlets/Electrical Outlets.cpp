9b562f3732dc1f7edb38444ec1dfd7d2494da51d
TheSebgon <92965117+TheSebgon@users.noreply.github.com>
2021-12-13 21:05:39 +0100

    Add files via upload

---
+++ Electrical Outlets/Electrical Outlets.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n, k, temp,sum=0;
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> k;
+        for (int j = 0; j < k; j++)
+        {
+            std::cin >> temp;
+             sum += (temp - 1);
+        }
+        std::cout << sum + 1 << std::endl;
+        sum = 0;
+    }
+}
---
+++ Reverse/Reverse.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    int* tab = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> tab[i];
+    }
+
+    for (int i = n-1; i >= 0; i--)
+    {
+        std::cout << tab[i] << std::endl;
+    }
+    delete[]tab;
+}
