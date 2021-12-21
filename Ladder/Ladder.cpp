bc7ed2a2e7088ae04928d8d08d950277fc9d3fc1
TheSebgon <92965117+TheSebgon@users.noreply.github.com>
2021-12-21 13:10:32 +0100

    Add files via upload

---
+++ Ladder/Ladder.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+	double high, angle;
+	double pi = 3.14159265359;
+	std::cin >> high >> angle;
+	angle = angle * (pi / 180);		//degree to radian
+	std::cout << std::ceil(high / sin(angle));
+}
